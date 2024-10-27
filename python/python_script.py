cf7037eed8c09c338a2cc0c5ed7fb5b6b57ebeb2
STAVROGIANNIS Christos <cstavrog@netcompany.com>
2024-10-27 15:05:19 +0200

    new approach

--- python/python_script.py
+++ python/python_script.py
@@ -1,21 +1,20 @@
+import sys
 from langchain_community.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
-from langchain.embeddings import OpenAIEmbeddings  
+from sentence_transformers import SentenceTransformer
 
-# loading the document
+# Load the Sentence Transformer model
+model = SentenceTransformer('all-MiniLM-L6-v2')  # You can choose other models as well
+
+# Loading
 loader = TextLoader('instruct.txt')
+index = VectorstoreIndexCreator().from_loaders([loader], embedding=model)
 
-# embedding staff
-embedding = OpenAIEmbeddings()  
-
-# index with  embedding
-index = VectorstoreIndexCreator(embedding=embedding).from_loaders([loader])
-
-#query from command line arguments
+# Get the query from command line arguments
 query = sys.argv[1]
 
-
+# Querying on the indexed document
 result = index.query(query)
 
-# result to send to server.js
+# Printing the result to send to server.js
 print(result)
